f2484d1b116aad94cb6faa91d0d18af9aea57629
Tatsuya Sato <satoryu.1981@gmail.com>
2025-01-04 16:26:22 +0900

    Just respond to a given greeting message 'hello'

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+@app.message("hello")
+def message_hello(message, say):
+    user = message['user']
+    say(f"Hello, <@{user}>!!")
+
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
