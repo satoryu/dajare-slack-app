5a08952102da7376a4fd465566a9616e57a66abf
Tatsuya Sato <satoryu.1981@gmail.com>
2025-01-04 16:43:17 +0900

    Completed Getting started

--- app.py
+++ app.py
@@ -7,7 +7,25 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 @app.message("hello")
 def message_hello(message, say):
     user = message['user']
-    say(f"Hello, <@{user}>!!")
+    say(
+      blocks=[
+        {
+          "type": "section",
+          "text": { "type": "mrkdwn", "text": f"Hey there <@{user}>!" },
+          "accessory": {
+            "type": "button",
+            "text": { "type": "plain_text", "text": "Click Me" },
+            "action_id": "button_click"
+          }
+        }
+      ],
+      text=f"Hey there <@{user}>!"
+    )
+
+@app.action("button_click")
+def action_button_click(body, ack, say):
+    ack()
+    say(f"<@{body['user']['id']}> clicked the button!")
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
